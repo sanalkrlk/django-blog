360db64407a2fe11d0c475cabc93b91efc461a55
sanalkrlk <abdurrahmankaraoluk@gmail.com>
2019-08-28 12:26:31 +0300

    Initial commit

---
+++ article/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+from .models import Article,Comment
+
+# Register your mpdels here.
+
+admin.site.register(Comment)
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+
+    list_display = ["title","author","created_date"]
+
+    list_display_links = ["title","created_date"]
+
+    search_fields = ["title"]
+
+    list_filter = ["title"]
+    class Meta:
+        Model = Article
+
